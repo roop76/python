e9ab1c22895c11209937058c01654c28d67ccad4
roop76 <roopigohri@gmail.com>
2024-08-19 18:47:23 +0530

    Add files via upload

--- if_else.py
+++ if_else.py
@@ -1,5 +1,36 @@
-num=int(input("Enter a number: "))
-if num%2==0:
-    print("even")
-else:
-    print("odd")
+# # write a programm to check a number whether it is odd or even.
+# num=int(input("Enter a number: "))
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+# num1=int(input("Enter the first number:"))
+# num2=int(input("Enter the second number:"))
+# num3=int(input("Enter the third number:"))
+# if num1>num2 and num1> num3:
+#     print("First number is greater:")
+# elif num2>=num1 and num2>=num3:
+#     print("Second number is greater:")
+# else:
+#     print("Third number is greater:")
+# Writing program for higher grade
+marks=float(input("Enter the marks: "))
+if marks>=90:
+    print("grade: A+ (excellent)")    
+elif marks>=80:    
+    print("grade: B+(medium)") 
+elif marks>=70:
+    print("grade: C+(average)")
+elif marks>=60:
+    print("Fail")
+
+
+
+
+
+
+
+ 
+ 
+
+
